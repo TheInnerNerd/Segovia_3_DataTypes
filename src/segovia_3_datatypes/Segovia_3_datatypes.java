2eb13fc2e7c3de196a2d59244acd45d22ae77db4
Mark Segovia <cujo935@gmail.com>
2015-09-30 06:09:09 -0500

    Data Types

---
+++ src/segovia_3_datatypes/Segovia_3_datatypes.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segovia_3_datatypes;
+
+/**
+ *
+ * @author TheInnerNerd
+ */
+public class Segovia_3_datatypes {
+
+    public static void main(String[] args) {
+        //This is a comment
+        //We wil use many comments
+        // to explain our code
+        //This program explores some of the
+        // promitive
+        int i = 0; 
+        float a = 0.01f;
+        double b = 0.01;
+        short c = 123;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        String e1 = "Mark is the bestest footbal player";
+        boolean f = true;
+        boolean g = 1 % 2 == 0;
+        
+        /*
+        here we will look at some mathematical
+        operators
+        */
+        // + this adds two numbers and more
+        // - this subracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows the remainder
+        //
+        
+        
+//        System.out.println(i + " an interger" );
+//        System.out.println(a + " a float" );
+//        System.out.println(b + " a double");
+//        System.out.println(c + " a short" );
+//        System.out.println(d1 + " a char" );
+//        System.out.println(d2 + " a char");
+//        System.out.println(d3 + " a char");
+//        System.out.println(d4 + " a char");
+//        System.out.println(e + " a string" );
+//        System.out.println(e1 + " a string" );
+//        System.out.println(f + " a boolean" );
+//        System.out.println(5 + " a boolean" );
+//        for(i = -123;i < 123; i++) {
+//            System.out.println((char) i);
+//            System.out.println(i % 3 ==0);
+//            }
+         //a numner value for char
+         //maps to a number
+    i = 0;
+    a = 0;
+        System.out.println(i + a + " an interger /" 
+                + "plus a float " );
+        System.out.println(e + i + " a float /"
+                + " plus a double");
+        System.out.println(b + c + " a double /"
+                + "plus a short");
+        System.out.println(c + " a short" );
+        
+    }
+   
+}
